093d1d0f902ab4d1a33791f776f03d731898a477
devtwocyphernate <ontimeway001@gmail.com>
2024-11-26 18:23:09 +0530

    First

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
+import { URL, fileURLToPath } from "node:url";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5174 },
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom", "bootstrap/dist/css/bootstrap.min.css"],
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "./build",
+      rollupOptions: {
+        external: "./src",
+      },
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
