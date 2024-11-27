87fcd9c0b4609c7712617168056360d937b71ced
devtwocyphernate <ontimeway001@gmail.com>
2024-11-27 11:49:33 +0530

    First

--- src/Components/MerchantSidebar/MerchantSidebar.jsx
+++ src/Components/MerchantSidebar/MerchantSidebar.jsx
@@ -5,6 +5,7 @@ import { FaAngleRight } from "react-icons/fa";
 import logo from "../../assets/logo.png";
 import dashboard from '../../assets/dashboard.svg'
 import subcription from '../../assets/subcription.svg'
+import parcel from '../../assets/parcel.svg'
 import "./MerchantSidebar.css";
 import transfer_1 from '../../assets/transfer 1.svg'
 
@@ -83,7 +84,7 @@ const MerchantSidebar = () => {
               onClick={() => toggleSubmenu(3)}
             >
               <span className="icon pe-4">
-                <img src={"/src/assets/parcel.svg"} style={{ width: "25px" }} alt="order" />
+                <img src={parcel} style={{ width: "25px" }} alt="order" />
               </span>
               Orders
               <span className={`arrow ${activeTab === 3 ? "rotate" : ""}`}>
