db3fcd3d79d834b7e4238a8b8b047cce46d178aa
devtwocyphernate <ontimeway001@gmail.com>
2024-11-27 10:34:45 +0530

    First

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     build: {
-      outDir: "./build",
+      outDir: "dist",
       rollupOptions: {
         external: "./src",
       },
